ae5fbd3a8a2c485528fcc8cf4d6f2da4b2103fe3
mateusz <mateusz@stake.fish>
2023-01-03 11:47:50 -0300

    build: library release process

---
+++ Makefile
@@ -0,0 +1,42 @@
+.PHONY: clean-pyc clean-build docs
+
+help:
+	@echo "clean-build - remove build artifacts"
+	@echo "clean-pyc - remove Python file artifacts"
+	@echo "format - formats files to match code style using black"
+	@echo "release - package and upload a release"
+	@echo "dist - package"
+
+clean: clean-build clean-pyc
+
+clean-build:
+	rm -fr build/
+	rm -fr dist/
+	rm -fr *.egg-info
+
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+
+format:
+	black .
+
+release: clean
+	# require that you be on a branch that's linked to upstream/master
+	git status -s -b | head -1 | grep "\.\.upstream/master"
+	./newsfragments/validate_files.py is-empty
+	# verify that docs build correctly
+	git config commit.gpgSign true
+	bumpversion $(bump)
+	git push upstream && git push upstream --tags
+	python setup.py sdist bdist_wheel
+	twine upload dist/*
+
+dist: clean
+	python setup.py sdist bdist_wheel
+	ls -l dist
+
+package: clean
+	python setup.py sdist bdist_wheel
+	python web3/scripts/release/test_package.py
---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+extras_require = {
+    "linter": [
+        "black==22.10.0",
+        "isort==5.10.1",
+    ],
+    "dev": [
+        "bumpversion",
+        "eth-brownie==1.19.2",
+        "pytest-asyncio==0.20.2",
+        "pytest-mock==3.10.0",
+        "setuptools>=38.6.0",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["linter"]
+    + extras_require["dev"]
+)
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+setup(
+    name="web3-utils",
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version="0.0.1",
+    description="""Stakefish web3 utils for Python""",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author="Michal Baranowski, Mateusz Sokola",
+    author_email="mbaranovski@stake.fish, mateusz@stake.fish",
+    url="https://github.com/stakefish/web3-utils",
+    include_package_data=True,
+    install_requires=[
+        "web3==5.31.1",
+    ],
+    python_requires="==3.10.5",
+    extras_require=extras_require,
+    py_modules=["web3_utils"],
+    # entry_points={"pytest11": ["pytest_ethereum = web3.tools.pytest_ethereum.plugins"]},
+    license="INTERNAL",
+    zip_safe=False,
+    keywords="ethereum, stakefish",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    # package_data={"web3": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: Proprietary :: Internal",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
