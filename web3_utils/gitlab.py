4fadc9979cb949292598118d2b2cc8fc426f6051
Michal Baranowski <baranovskimichal@gmail.com>
2025-04-01 18:31:40 -0400

    feat: filter gitlab files by regex

--- web3_utils/gitlab.py
+++ web3_utils/gitlab.py
@@ -1,20 +1,23 @@
 import base64
+import re
 import shutil
 from os import path, makedirs, getcwd
 from gitlab import Gitlab
-
+from typing import Optional
 
 class GitLab:
     def __init__(self, url: str, token: str, tmp_dir: str = path.join(getcwd(), "tmp", "public_keys")):
         self.client = Gitlab(private_token=token, url=url)
         self.tmp_dir = tmp_dir
 
-    def download_files_from_project(self, project_id: int, dir_path: str, branch: str = "master"):
+    def download_files_from_project(self, project_id: int, dir_path: str, branch: str = "master", include_only_files: Optional[list[str]] = None):
         # ensures that access token doesn't expire
         self.client.auth()
 
         project = self.client.projects.get(project_id)
         items = project.repository_tree(ref=branch, path=dir_path, get_all=True)
+        if include_only_files:
+            items = [item for item in items if any(re.match(pattern, item["name"]) for pattern in include_only_files)]
 
         self._prepare_temp_directory()
 
