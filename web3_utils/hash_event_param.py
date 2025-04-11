268f306d0f478375ec2f4ca964bc92c5e159af5f
Michal Baranowski <29602306+mbaranovski@users.noreply.github.com>
2025-04-11 14:51:18 +0400

    feat: update deps (#22)

--- web3_utils/hash_event_param.py
+++ web3_utils/hash_event_param.py
@@ -2,4 +2,4 @@ from web3 import Web3
 
 
 def hash_event_param(param: str):
-    return str(Web3.toHex(Web3.solidityKeccak(["bytes"], [param])))
+    return str(Web3.to_hex(Web3.solidity_keccak(["bytes"], [param])))
